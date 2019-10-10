3432e4cc68c2f3b21068eb9b2300a4fc206b124a
pawlo9666 <pawlo9666@gmail.com>
2019-10-10 18:18:29 +0200

    info

---
+++ test.py
@@ -0,0 +1,6 @@
+# komendy w gicie
+# git add nazwapliku
+# git commit -m "komentarz tu"
+# git push
+# git branch // nazwa stworzy nowy branch // -d usunie branmch
+# git checkout // zmiana brancha // sprawdzenie aktywnego brancha
