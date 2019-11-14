540202b637e47ea42e41efa57e5cf5015884d3ad
Paweł Skiba <35065500+pawlo9666@users.noreply.github.com>
2019-11-14 10:26:53 +0100

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-# komendy w gicie
-# git add nazwapliku
-# git commit -m "komentarz tu"
-# git push
-# git branch // nazwa stworzy nowy branch // -d usunie branmch
-# git checkout // zmiana brancha // sprawdzenie aktywnego brancha
