08f409a414a07609f3a293286568a645038c8cf6
Scyzoryk <damianszefler96@gmail.com>
2019-12-01 11:24:35 +0100

    ???

--- main-project/KurierWWW/trackparc/admin.py
+++ main-project/KurierWWW/trackparc/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import *
 
-
+admin.site.register(Adres)
 admin.site.register(Klient)
+admin.site.register(DaneKlient)
+admin.site.register(Zlecenie)
+admin.site.register(Kierowcy)
+admin.site.register(Przydzial)
